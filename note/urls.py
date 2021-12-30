461354a39f266bbe96bbf72104e59e46464aa37a
reyzacaloh <reyza.abyan@gmail.com>
2021-12-31 03:01:36 +0700

    flutter_note

--- note/urls.py
+++ note/urls.py
@@ -7,4 +7,9 @@ urlpatterns = [
     path("", index, name="index"),
     path('add_message/', add_message, name="add_message"),
     path('<str:id>/delete', delete_message, name="delete_message"),
+    path("flutter/list-note/<str:name>", flutter_list_note, name="flutter/list-note"),
+    path("flutter/detail-note/<str:name>/<str:id>", flutter_detail_note, name="flutter/edit-note"),
+    path("flutter/delete-note/", flutter_delete_note, name="flutter/delete-note"),
+    path("flutter/add-note/", flutter_add_note, name="flutter/add-note"),
+    path("flutter/edit-note/", flutter_edit_note, name="flutter/edit-note"),
 ]
--- note/views.py
+++ note/views.py
@@ -8,7 +8,8 @@ from django.http import HttpResponseRedirect,  JsonResponse
 from django.template.loader import render_to_string 
 from user.models import Profile
 from django.contrib.auth.models import User
-
+from django.views.decorators.csrf import csrf_exempt
+import json
 # Create your views here.
 
 kelas_note = ["rotate-1 yellow-bg", "rotate-1 lazur-bg", "rotate-1 red-bg", "rotate-1 navy-bg",  "rotate-2 lazur-bg", "rotate-2 red-bg", "rotate-2 navy-bg", "rotate-2 yellow-bg"]
@@ -68,6 +69,89 @@ def delete_message (request,id):
         data['html_form'] = render_to_string('note/note_del.html',context,request=request)
 
     return JsonResponse(data)
+
+
+@csrf_exempt
+def flutter_list_note(request, name):
+    user_id = request.user.id  # Get user id from request
+    messages = NoteModel.objects.filter(user=Profile.objects.get(name=name).user).all().values()
+    messages_list = list(messages)
+    if messages_list is not None:
+        print(messages_list)
+        return JsonResponse(messages_list, safe=False, status=200)
+    else:
+        return JsonResponse({}, status=404)
+
+@csrf_exempt
+def flutter_edit_note(request):
+    data = json.loads(request.body)
+    name = data['username']
+    id = int(data['id'])
+    user = User.objects.get(username=name)
+    
+    
+    if user is not None:
+        user_message = NoteModel.objects.get(
+            user=user, id=id
+        )
+        user_message.message = data["message"]
+        user_message.title = data['title']
+        user_message.timestamp = data['timestamp']
+        user_message.save()
+        
+        return JsonResponse({}, status=200)
+    else:
+        return JsonResponse({}, status=404)
+
+@csrf_exempt
+def flutter_detail_note(request,id,name):
+    messages = NoteModel.objects.filter(user=Profile.objects.get(name=name).user).all().values()
+    messages_list = list(messages)
+    num = 0
+    
+    for x in messages_list:
+        if x['id'] == int(id) :
+           
+            return JsonResponse(messages_list[num], status=200)
+        num += 1
+    return JsonResponse({}, status=404)
+
+@csrf_exempt
+def flutter_delete_note(request):
+    data = json.loads(request.body)
+    name = data['username']
+    id = int(data['id'])
+    user = User.objects.get(username=name)
+    
+    if user is not None:
+        user_message = NoteModel.objects.get(
+            user=user, id=id
+        )
+        user_message.delete()
+        return JsonResponse({}, status=200)
+    else:
+        return JsonResponse({}, status=404)
+
+@csrf_exempt
+def flutter_add_note(request):
+    data = json.loads(request.body)
+    name = data["username"]
+    title = data['title']
+    timestamp = data["timestamp"]
+    message = data['message']
+    user = User.objects.get(username= name) 
+    
+    
+    try:
+        note = NoteModel.objects.create(title = title,
+                                 message=message,
+                                 timestamp=timestamp,
+                                 user = user)
+        note.save()
+        
+        return JsonResponse({}, status=200)
+    except Exception as e:
+        return JsonResponse({}, status=404)
 
 
 
