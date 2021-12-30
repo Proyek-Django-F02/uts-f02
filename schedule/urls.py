52c8289d51438f52123b6c951e5458c3c62af718
Isyah Auliarahmani Rafifa <isyah.auliarahmani@ui.ac.id>
2021-12-31 03:58:37 +0700

    add flutter_activity_list in schedule views

--- schedule/urls.py
+++ schedule/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import schedule, delete_activity
+from .views import schedule, delete_activity, flutter_activity_list
 # from schedule import views as schedule
 
 urlpatterns = [
     path('', schedule, name='schedule'),
     path('delete/', delete_activity, name='delete'),
+    path('flutter/activity-list/<str:name>', flutter_activity_list, name='flutter/schedule')
 ]
